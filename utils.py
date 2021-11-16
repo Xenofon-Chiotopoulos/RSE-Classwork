b74b90d2d6bcc065c4c0a1ceb813c51383c737f1
Xenofon-Chiotopoulos <92038142+Xenofon-Chiotopoulos@users.noreply.github.com>
2021-11-16 20:33:07 +0000

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-def haversine_distance(lat1, lon1, lat2, lon2):
-    """Return the distance in km between two points around the Earth.
-
-    Latitude and longitude for each point are given in degrees.
-    """
-    raise NotImplementedError
